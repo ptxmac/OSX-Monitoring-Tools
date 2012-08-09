1f63843155534eafeb69b34864df35fc0d8f2541
Peter Kristensen <ptx@rocketbrothers.dk>
2012-08-09 15:36:47 +0200

    assume smartctl is in PATH

--- check_smart.sh
+++ check_smart.sh
@@ -16,10 +16,10 @@
 # 	We use this on almost all our monitored Macs for the obvious reasons. It has saved us from headaches more than a few times by getting on top of a dying drive in time.
 
 # enable SMART on the drive if it is not already
-smartoncmd=`/opt/local/libexec/nagios/smartctl --smart=on $1`
+smartoncmd=`smartctl --smart=on $1`
 
 # run smartctl on disk and report on output
-if echo `/opt/local/libexec/nagios/smartctl -H $1` | grep -q "PASSED"
+if echo `smartctl -H $1` | grep -q "PASSED"
 	then
   		printf "OK - SMART Passed\n"
   		exit 0
