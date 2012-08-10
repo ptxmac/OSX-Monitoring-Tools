a742faf723928c9842c2e23f494514e38763b987
Peter Kristensen <ptx@rocketbrothers.dk>
2012-08-10 13:23:25 +0200

    Updated to GNTP

--- notify_growl.pl
+++ notify_growl.pl
@@ -1,13 +1,14 @@
 #!/usr/bin/perl -w
 #
-# Created by Mathieu GagnÃ© 2009
+# Created by Mathieu Gagne 2009
+# Updated by Guillaume Bisch, 2012
 #
- 
+
 use strict;
 use warnings;
-use Net::Growl;
+use Growl::GNTP;
 use Getopt::Long qw(:config no_ignore_case bundling);
- 
+
 # Default values
 my $application = 'Nagios';
 my $title = 'Alert';
@@ -16,86 +17,73 @@ my $priority = 2;
 my $sticky = 0;
 my $destination = 'localhost';
 my $password = '';
- 
+
 my $help = 0;
- 
+
 my $pod2usage = sub {
-  # Load Pod::Usage only if needed.
-  require "Pod/Usage.pm";
-  import Pod::Usage;
- 
-        pod2usage(@_);
+    # Load Pod::Usage only if needed.
+    require "Pod/Usage.pm";
+    import Pod::Usage;
+    pod2usage(@_);
 };
- 
+
 # Declare and retreive options
 GetOptions(
-  'h|help'          => \$help,
-  'a|application=s' => \$application,
-  't|title=s'       => \$title,
-  'm|message=s'     => \$message,
-  'P|priority=i'    => \$priority,
-  's|sticky'        => \$sticky,
-  'H|host=s'        => \$destination,
-  'p|password=s'    => \$password,
+    'h|help' => \$help,
+    'a|application=s' => \$application,
+    't|title=s' => \$title,
+    'm|message=s' => \$message,
+    'P|priority=i' => \$priority,
+    's|sticky' => \$sticky,
+    'H|host=s' => \$destination,
+    'p|password=s' => \$password,
 ) or $pod2usage->(1);
- 
+
 # Print help
 $pod2usage->(1) if $help;
- 
+
 # Validate options
 if ( $application eq '' ) {
-  die "Error: Missing mandatory option: application\n";
+die "Error: Missing mandatory option: application\n";
 }
- 
+
 if ( $title eq '' ) {
-  die "Error: Missing mandatory option: title\n";
+    die "Error: Missing mandatory option: title\n";
 }
- 
+
 if ( $message eq '' ) {
-  die "Error: Missing mandatory option: message\n";
+    die "Error: Missing mandatory option: message\n";
 }
- 
+
 if ( $priority eq '' ) {
-  die "Error: Missing mandatory option: priority\n";
+    die "Error: Missing mandatory option: priority\n";
 }
- 
-if ( $password eq '' ) {
-  die "Error: Missing mandatory option: password\n";
-}
- 
+
+# if ( $password eq '' ) {
+#     die "Error: Missing mandatory option: password\n";
+# }
+
 #
 # Main program
 #
- 
-# Set up the Socket
-my %addr = (
-  PeerAddr => $destination,
-  PeerPort => Net::Growl::GROWL_UDP_PORT,
-  Proto    => 'udp',
+
+my $growl = Growl::GNTP->new(
+    AppName => $application,
+    PeerHost => $destination,
+    Password => $password
 );
- 
-my $s = IO::Socket::INET->new ( %addr ) || die "Could not create socket: $!\n";
- 
+
 # Register the application
-my $p = Net::Growl::RegistrationPacket->new(
-  application => $application,
-  password    => $password,
+$growl->register([
+    { Name => $application, },
+]);
+
+# Send the notification
+$growl->notify(
+    Event => "Nagios",
+    Title => $title,
+    Message => $message,
+    Icon => "",
+    Priority => $priority,
+    Sticky => $sticky
 );
- 
-$p->addNotification();
- 
-print $s $p->payload();
- 
-# Send a notification
-$p = Net::Growl::NotificationPacket->new(
-  application => $application,
-  title       => $title,
-  description => $message,
-  priority    => $priority,
-  sticky      => $sticky,
-  password    => $password,
-);
- 
-print $s $p->payload();
- 
-close($s);
